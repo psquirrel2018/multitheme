ea0fc3c3d3a56f36d9523cce2219d4c13a16ce6b
munts <scott@confluence.solutions>
2018-06-14 09:24:10 -0600

    fixed a bug that was preventing my custom fields from showing on the front-page.

--- wp-content/themes/cws_bikeshop/lib/front-page-functions.php
+++ wp-content/themes/cws_bikeshop/lib/front-page-functions.php
@@ -21,55 +21,11 @@ if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
 	require_once dirname( __FILE__ ) . '/CMB2/init.php';
 }
 
-/**
- * Conditionally displays a metabox when used as a callback in the 'show_on_cb' cmb2_box parameter
- *
- * @param  CMB2 object $cmb CMB2 object
- *
- * @return bool             True if metabox should show
- */
-function multi_confluence_show_if_front_page( $cmb ) {
-	// Don't show this metabox if it's not the front page template
-	if ( $cmb->object_id !== get_option( 'page_on_front' ) ) {
-		return false;
-	}
-	return true;
-}
-
-/**
- * Conditionally displays a field when used as a callback in the 'show_on_cb' field parameter
- *
- * @param  CMB2_Field object $field Field object
- *
- * @return bool                     True if metabox should show
- */
-function yourprefix_hide_if_no_cats( $field ) {
-	// Don't show this field if not in the cats category
-	if ( ! has_tag( 'cats', $field->object_id ) ) {
-		return false;
-	}
-	return true;
-}
-
-/**
- * Conditionally displays a message if the $post_id is 2
- *
- * @param  array             $field_args Array of field parameters
- * @param  CMB2_Field object $field      Field object
- */
-function yourprefix_before_row_if_2( $field_args, $field ) {
-	if ( 2 == $field->object_id ) {
-		echo '<p>Testing <b>"before_row"</b> parameter (on $post_id 2)</p>';
-	} else {
-		echo '<p>Testing <b>"before_row"</b> parameter (<b>NOT</b> on $post_id 2)</p>';
-	}
-}
-
-add_action( 'cmb2_admin_init', 'multi_confluence_register_custom_metabox' );
+add_action( 'cmb2_admin_init', 'multitheme_register_custom_metabox' );
 /**
  * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
  */
-function multi_confluence_register_custom_metabox()
+function multitheme_register_custom_metabox()
 {
 
     // Start with an underscore to hide fields from custom fields list
@@ -82,8 +38,8 @@ function multi_confluence_register_custom_metabox()
         'id' => $prefix . 'metabox',
         'title' => __('Homepage Custom Fields', 'cmb2'),
         'object_types' => array('page',), // Post type
-        'show_on'      => array( 'key' => 'page-template', 'value' => 'front-page.php' ),
-        //'show_on_cb' => '_multi_confluence_frontpage_show_if_front_page', // function should return a bool value
+        //'show_on'      => array( 'key' => 'page-template', 'value' => 'front-page.php' ),
+        'show_on_cb' => '_multitheme_frontpage_show_if_front_page', // function should return a bool value
         // 'context'    => 'normal',
         // 'priority'   => 'high',
         // 'show_names' => true, // Show field names on the left
